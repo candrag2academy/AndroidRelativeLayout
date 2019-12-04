1d263422b880a9d746f3687e546c00451cc13bc0
candra <candra@g2academy.co>
2019-12-04 13:33:02 +0700

    init

---
+++ app/src/main/java/co/g2lab/relativelayout/MainActivity.java
@@ -0,0 +1,14 @@
+package co.g2lab.relativelayout;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+public class MainActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+    }
+}
